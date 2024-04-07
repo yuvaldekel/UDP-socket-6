202d9163f720d16d35f8c5dadebbf81ee6ed6ec5
Yuval Dekel <yuvddd05@gmail.com>
2024-04-07 18:32:10 +0300

    add pyc server

--- UDP_client.py
+++ UDP_client.py
@@ -2,9 +2,12 @@ import socket
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-        my_socket.sendto('Hello'.encode(), ("127.0.0.1", 8821))
+        what_to_send = ''
+        while what_to_send == '':
+            what_to_send = input("what would you like to send to the server: ")
+        my_socket.sendto(what_to_send.encode(), ("127.0.0.1", 8821))
         data, remote_address = my_socket.recvfrom(1024)
-        print("The server '{}' sent: {}".format(remote_address, data.decode()))
+        print("The server '{}' sent: {}".format(remote_address[0], data.decode()))
 
 if __name__ == "__main__":
     main()
